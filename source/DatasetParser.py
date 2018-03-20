c21ddc5d50d5238aa331167d884d9dda16717e3f
S.Jacques <s-jacques@live.com>
2018-03-20 17:51:19 -0400

    Refactored the code to account for new messages

--- source/DatasetParser.py
+++ source/DatasetParser.py
@@ -1,271 +1,361 @@
 import operator
-import sys
-import re
-import nltk
+import pickle
 
 from nltk import tokenize
 from nltk.sentiment.util import *
 
 
 class Parser:
-    def __init__(self):
-        self.dict = {}
-        self.data_file = None
-        self.stopwords_file = None
-        self.alphabet_file = None
-        self.dict_file = None
-        self.char_file = None
-        self.word_file = None
-        self.updated_dict_file = None
-        self.final_parse_file = None
-
     def generate_wordset(self, data_filename, word_filename):
+        print("Generating Wordset")
 
-        # Load required file
-        self.data_file = open(data_filename, "r", encoding="utf-8")
-
-        # Read file and split contents
-        content = self.data_file.read()
-        data = content.split()
-        unique_words = set(data)
+        # Load required file and split content
+        data_file = open(data_filename, "r", encoding="utf-8")
+        data = set(data_file.read().split())
+        data_file.close()
 
         # Create wordset
-        output = "Count: " + str(len(unique_words)) + "\n\n"
-        for word in unique_words:
+        output = "Count: " + str(len(data)) + "\n\n"
+        for word in data:
             output += word + "\n"
 
         # Output wordset to file
-        self.word_file = open(word_filename, 'w', encoding="utf-8")
-        self.word_file.write(output)
-        self.word_file.close()
-        print("Wordset created")
+        word_file = open(word_filename, 'w', encoding="utf-8")
+        word_file.write(output)
+        word_file.close()
+
+        print("Wordset Generated")
 
     def generate_charset(self, data_filename, char_filename):
+        print("Generating Charset")
 
-        # Load required file
-        self.data_file = open(data_filename, "r", encoding="utf-8")
-
-        # Read file and split contents
-        content = self.data_file.read()
-        unique_chars = set(content)
+        # Load required file and split content
+        data_file = open(data_filename, "r", encoding="utf-8")
+        data = set(data_file.read())
+        data_file.close()
 
         # Create charset
-        output = "Count: " + str(len(unique_chars)) + "\n\n"
-        for char in unique_chars:
+        output = "Count: " + str(len(data)) + "\n\n"
+        for char in data:
             output += char + "\n"
 
         # Output charset to file
-        self.char_file = open(char_filename, 'w', encoding="utf-8")
-        self.char_file.write(output)
-        self.char_file.close()
-        print("Charset created")
+        char_file = open(char_filename, 'w', encoding="utf-8")
+        char_file.write(output)
+        char_file.close()
 
-    def generate_first_parse(self, data_filename, stopwords_filename, parsed_filename, sentiments_filename, alphabet_filename):
+        print("Charset Generated")
 
-        # Load required files to do the parsing
-        self.data_file = open(data_filename, "r", encoding="utf-8")
-        self.alphabet_file = open(alphabet_filename, "r", encoding="utf-8")
-        self.stopwords_file = open(stopwords_filename, "r", encoding="utf-8")
+    def generate_first_parse(self, data_filename, alphabet_filename, stopwords_filename, parsed_filename, is_transport):
+        # print starting message
+        if is_transport:
+            print("Generating Transport First Parse")
+        else:
+            print("Generating Sentiment First Parse")
 
-        # Read the files and split contents
-        content = self.data_file.read()
-        lines = content.split("\n")
-        alphabet = self.alphabet_file.read().split(",")
-        french_stopwords = self.stopwords_file.read().split("\n")
-        print(french_stopwords)
+        # Load required file and split content
+        data_file = open(data_filename, "r", encoding="utf-8")
+        data = data_file.read().split("\n")
+        data_file.close()
 
         # Prepare variables
         output = ""
-        sentiments = ""
-        percent = 0
-        iterations = 0
+        percent = iterations = 0
+        unk_ctr = kno_ctr = neu_ctr = mix_ctr = neg_ctr = pos_ctr = 0
+        random.shuffle(data)
+
         print("0%")
 
         # Create parsed file
-        for line in lines:
+        for line in data:
             iterations += 1
 
-            if iterations % int(len(lines) / 100) == 0:
+            # Update progress
+            if iterations % int(len(data) / 100) == 0:
                 percent += 1
                 print(str(percent) + "%")
 
             try:
                 sentiment, message = line.split(",\"")
-                sentiments += sentiment + "\n"
-                sentiment = str.lower(sentiment)
 
-                # If line is not starting by a sentiment, skip it
-                if sentiment != "positif" \
-                        and sentiment != "negatif" \
-                        and sentiment != "neutre"\
-                        and sentiment != "mixposneg"\
-                        and sentiment != "inconnu":
-                    continue
+                # Rename sentiments and increase counters
+                if is_transport:
+                    if sentiment != "INCONNU":
+                        sentiment = "KNO"
+                        kno_ctr += 1
+                    else:
+                        sentiment = "UNK"
+                        unk_ctr += 1
+                else:
+                    if sentiment == "POSITIF":
+                        pos_ctr += 1
+                        sentiment = "POS"
+                    elif sentiment == "NEGATIF":
+                        neg_ctr += 1
+                        sentiment = "NEG"
+                    elif sentiment == "MIXPOSNEG":
+                        mix_ctr += 1
+                        sentiment = "MIX"
+                    elif sentiment == "NEUTRE":
+                        neu_ctr += 1
+                        sentiment = "NEU"
+                    else:
+                        continue
 
-                # Remove URLs
-                message = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', message, flags=re.MULTILINE)
-
-                # Remove unintentional skiplines
-                message = re.sub(r'\[[^\]]*\]', '', message)
-
-                # Change all characters to lower case
-                message = str.lower(str(message))
-
-                message_list = list(message)
-                new_message = ""
-
-                for char in message_list:
-                    if char in alphabet:
-                        new_message += char
-
-                word_list = tokenize.word_tokenize(new_message, language='french')
-
-                new_message = ""
-
-                for word in word_list:
-                    # Remove character repetitions
-                    word = re.sub(r'([a-z])\1+', r'\1', word)
-
-                    if word not in french_stopwords:
-                        new_message += word + ","
-
-                output += new_message[:-1] + "\n"
+                # Clean message and add to output with sentiment
+                output += sentiment + "," + self.clean_message(message, alphabet_filename, stopwords_filename) + "\n"
             except ValueError:
                 pass
 
         # Output to parsed file
         parsed_file = open(parsed_filename, 'w', encoding="utf8")
-        parsed_file.write(output)
+        parsed_file.write(output[:-1])
         parsed_file.close()
 
-        # Sentiments to sentiments file
-        sentiment_file = open(sentiments_filename, "w", encoding="utf-8")
-        sentiment_file.write(sentiments)
-        sentiment_file.close()
-        print("File parsed")
+        # Print completion message
+        if is_transport:
+            print("Transport First Parse Generated")
+            print("\tKnown: " + str(unk_ctr))
+            print("\tUnknown: " + str(kno_ctr))
+        else:
+            print("Sentiment First Parse Generated")
+            print("\tMixed: " + str(mix_ctr))
+            print("\tNegative: " + str(neg_ctr))
+            print("\tNeutral: " + str(neu_ctr))
+            print("\tPositive: " + str(pos_ctr))
 
-    def first_dictionary(self, parsed_filename, dict_filename):
+    def generate_dictionary(self, parsed_filename, dict_filename, is_transport):
+        # Print starting message
+        if is_transport:
+            print("Generating Transport Dictionary")
+        else:
+            print("Generating Sentiment Dictionary")
+
+        # Load required file and split content
         parsed_file = open(parsed_filename, "r", encoding="utf-8")
-        content = str.lower(parsed_file.read())
+        data = str.lower(parsed_file.read()).split("\n")
+        parsed_file.close()
 
-        lines = content.split("\n")
-        output = "count," + str(len(lines)) + "\n\n"
-
-        for line in lines[1:]:
-            words = line.split(",")
-
-            for word in words[1:]:
-                if word in self.dict:
-                    self.dict[word] += 1
-                else:
-                    self.dict[word] = 1
-
-        self.dict = sorted(self.dict.items(), key=operator.itemgetter(1), reverse=True)
-
-        for word, count in self.dict:
-            output += word + "," + str(count) + "\n"
-
-        self.dict_file = open(dict_filename, "w", encoding="utf-8")
-        self.dict_file.write(output)
-
-        print("Dictionary created")
-
-    def second_parse(self, parse_filename, final_parse_filename, updated_dict_filename):
-        self.final_parse_file = open(final_parse_filename, 'w', encoding="utf-8")
-        self.updated_dict_file = open(updated_dict_filename, 'r', encoding="utf-8")
-
-        allowed_words = []
-
-        dict_content = self.updated_dict_file.read()
-        dict_lines = dict_content.split("\n")
-
-        for line in dict_lines:
-            allowed_words.append(line.split(",")[0])
-
-        parse_file = open(parse_filename, 'r', encoding="utf-8")
-        content = parse_file.read()
-        parse_file.close()
-
-        lines = content.split("\n")
-
-        new_content = ""
-        percent = 0
+        # Prepare variables
+        dictionary = {}
+        iterations = percent = 0
 
         print("0%")
 
-        iterations = 0
-
-        for line in lines:
+        # Create dictionary
+        for line in data:
             iterations += 1
 
-            if iterations % int(len(lines) / 100) == 0:
+            # Update progress
+            if iterations % int(len(data) / 100) == 0:
                 percent += 1
                 print(str(percent) + "%")
 
             words = line.split(",")
-            new_words = []
 
-            for word in words:
-                if word in allowed_words:
-                    new_words.append(word)
+            for word in words[1:]:
+                # Add words to dictionary or increase count if already in there
+                if word in dictionary:
+                    dictionary[word] += 1
+                else:
+                    dictionary[word] = 1
 
-            new_words = mark_negation(new_words)
-            new_line = ""
+        # Sort dictionary in order of most popular words
+        dictionary = sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True)
 
-            for word in new_words:
-                new_line += word + ","
+        output = ""
 
-            new_content += new_line[:-1] + "\n"
+        for word, count in dictionary:
+            output += word + "," + str(count) + "\n"
 
-        self.final_parse_file.write(new_content[:-1])
+        # Output to dictionary file
+        dict_file = open(dict_filename, "w", encoding="utf-8")
+        dict_file.write(output[:-1])
+        dict_file.close()
 
-        print("Finished second parse")
+        # Print completion message
+        if is_transport:
+            print("Transport Dictionary Generated")
+        else:
+            print("Sentiment Dictionary Generated")
 
-    def generate_featuresrets(self, parsed_filename, dict_filename, sentiments_filename, featuresets_filename):
+    # PROBLEM HERE!!!!
+    def generate_second_parse(self, parse_filename, final_parse_filename, updated_dict_filename, is_transport):
+        # Print starting message
+        if is_transport:
+            print("Generating Transport Second Parse")
+        else:
+            print("Generating Sentiment Second Parse")
+
+        # Load required files and split content
+        parse_file = open(parse_filename, 'r', encoding="utf-8")
+        data = parse_file.read().split("\n")
+        parse_file.close()
+
+        # Prepare variables
+        output = ""
+        percent = iterations = 0
+
+        print("0%")
+
+        # Create parsed file
+        for line in data:
+            iterations += 1
+
+            # Track progress
+            if iterations % int(len(data) / 100) == 0:
+                percent += 1
+                print(str(percent) + "%")
+
+            words = line.split(",")
+
+            message = ""
+
+            for word in words[1:]:
+                message += word + ","
+
+            # Remove unwated words and add them to the output
+            output += words[0] + "," + self.remove_unwated_words(message[:-1], updated_dict_filename) + "\n"
+
+        # Output to parsed file
+        final_parse_file = open(final_parse_filename, 'w', encoding='utf-8')
+        final_parse_file.write(output[:-1])
+        final_parse_file.close()
+
+        # Print completion message
+        if is_transport:
+            print("Transport Second Parse Generated")
+        else:
+            print("Sentiment Second Parse Generated")
+
+    def learn_naive_bayes(self, parsed_filename, dict_filename, nb_classifier_filename):
+        # Load required files and split content
         data_file = open(parsed_filename, 'r', encoding='utf-8')
-        dict_file = open(dict_filename, 'r', encoding='utf-8')
-        sent_file = open(sentiments_filename, 'r', encoding='utf-8')
-
         data_lines = data_file.read().split("\n")
+        data_file.close()
+        dict_file = open(dict_filename, 'r', encoding='utf-8')
+        dict_data = dict_file.read().split("\n")
+        dict_file.close()
+
+        # Prepare variables
+        iterations = percent = 0
+        featuresets = []
         data = []
 
         for line in data_lines:
             data.append(line.split(","))
 
-        sent_lines = sent_file.read().split("\n")
-        all_words = set()
+        # Add words to a set
+        words = set()
+        for line in dict_data:
+            words.add(line.split(",")[0])
 
-        for line in dict_file.read().split("\n")[:2000]:
-            all_words.add(line.split(",")[0])
+        print("Generating Featuresets\n0%")
 
-        featuresets = []
-
-        iterations = 0
-        percent = 0
-
-        print("0%")
-
-        for i in range(0, len(data_lines)):
+        for datum in data:
             iterations += 1
 
+            # Track progress
             if iterations % int(len(data_lines) / 100) == 0:
                 percent += 1
                 print(str(percent) + "%")
 
-            featuresets.append((self.document_features(data[i], all_words), sent_lines[i]))
+            featuresets.append((self.document_features(datum[1:], words), datum[0]))
+
+        print("Featuresets Generated")
 
         data_size = len(data_lines)
         train_size = int(data_size * 0.8)
 
-        random.shuffle(featuresets)
-        training_set = featuresets[:train_size]
+        training_set = featuresets[0:train_size]
         testing_set = featuresets[train_size:data_size]
+        featuresets.clear()
+
+        print("Training Naive Bayes Classifier with Training Set of Size " + str(len(training_set)))
 
         classifier = nltk.NaiveBayesClassifier.train(training_set)
+        training_set.clear()
 
-        classifier.show_most_informative_features()
+        print("Naive Bayes Classifier Trained")
 
-        print(nltk.classify.accuracy(classifier, testing_set))
+        classifier.show_most_informative_features(20)
+
+        print("Testing Accuracy with Testing Set of size " + str(len(testing_set)))
+        print("Accuracy: " + str(nltk.classify.accuracy(classifier, testing_set)))
+        testing_set.clear()
+
+        nb_classifier_file = open(nb_classifier_filename, 'wb')
+        pickle.dump(classifier, nb_classifier_file)
+        nb_classifier_file.close()
+
+    @staticmethod
+    def clean_message(message, alphabet_filename, stopwords_filename):
+        # Load required files and split content
+        alphabet_file = open(alphabet_filename, "r", encoding="utf-8")
+        alphabet = alphabet_file.read().split(",")
+        alphabet_file.close()
+        stopwords_file = open(stopwords_filename, "r", encoding="utf-8")
+        french_stopwords = stopwords_file.read().split("\n")
+        stopwords_file.close()
+
+        # Remove URLs
+        message = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', message, flags=re.MULTILINE)
+
+        # Remove unintentional skiplines
+        message = re.sub(r'\[[^\]]*\]', '', message)
+
+        # Change all characters to lower case
+        message = str.lower(str(message))
+
+        message_list = list(message)
+        new_message = ""
+
+        for char in message_list:
+            # Remove unwanted characters
+            if char in alphabet:
+                new_message += char
+
+        # Tokenize words
+        word_list = tokenize.word_tokenize(new_message, language='french')
+
+        new_message = ""
+
+        for word in word_list:
+            # Remove character repetitions
+            word = re.sub(r'([a-z])\1+', r'\1', word)
+
+            # Remove unwanted words
+            if word not in french_stopwords:
+                new_message += word + ","
+
+        return new_message[:-1]
+
+    @staticmethod
+    def remove_unwated_words(line, dictionary_filename):
+        # Open required file and split content
+        updated_dict_file = open(dictionary_filename, 'r', encoding="utf-8")
+        dict_data = updated_dict_file.read().split("\n")
+        updated_dict_file.close()
+
+        # Store allowed words in a list
+        allowed_words = []
+        for line in dict_data:
+            allowed_words.append(line.split(",")[0])
+
+        words = line.split(",")
+        new_words = []
+
+        for word in words:
+            # Check if word is in updated dictionary
+            if word in allowed_words:
+                new_words.append(word)
+
+        new_line = ""
+
+        for word in new_words:
+            new_line += word + ","
+
+        return new_line[:-1]
 
     @staticmethod
     def document_features(document, all_words):
@@ -278,12 +368,3 @@ class Parser:
                 features[word] = False
 
         return features
-
-    def closefiles(self):
-        if self.data_file: self.data_file.close()
-        if self.alphabet_file: self.alphabet_file.close()
-        if self.dict_file: self.dict_file.close()
-        if self.char_file: self.char_file.close()
-        if self.word_file: self.word_file.close()
-        if self.updated_dict_file: self.updated_dict_file.close()
-        if self.final_parse_file: self.final_parse_file.close()
--- source/Main.py
+++ source/Main.py
@@ -1,45 +1,65 @@
 from source.DatasetParser import Parser
 import platform
 
+
+dataset_file = "../datasets/deft_dataset.csv"
+wordset_file = "../wordsets/deft_wordset.csv"
+charset_file = "../charsets/deft_charset.csv"
+transport_parse_file = "../parsed/deft_parse_transport.csv"
+sentiment_parse_file = "../parsed/deft_parse_sentiment.csv"
 alphabet_file = "../alphabets/deft_alphabet.csv"
 stopwords_file = "../stopwords/deft_stopwords.csv"
-charset_file = "../charsets/deft_charset.csv"
-dataset_file = "../datasets/deft_dataset.csv"
-dictionary_file = "../dictionaries/deft_dictionary.csv"
-updated_dictionary_file = "../dictionaries/deft_dictionary_updated.csv"
-sentiments_file = "../sentiments/deft_sentiments.csv"
-parsed_file = "../parsed_datasets/deft_parse.csv"
-stopwords_file = "../stopwords/deft_stopwords.csv"
-wordset_file = "../wordsets/deft_wordset.csv"
-final_parse_file = "../parsed_datasets/final_deft_parse.csv"
-featuresets_file = "../featuresets/deft_featuresets.csv"
-
-print(platform.architecture())
+transport_dictionary_file = "../dictionaries/deft_dictionary_transport.csv"
+sentiment_dictionary_file = "../dictionaries/deft_dictionary_sentiment.csv"
+transport_updated_dictionary_file = "../dictionaries/updated_deft_dictionary_transport.csv"
+sentiment_updated_dictionary_file = "../dictionaries/updated_deft_dictionary_sentiment.csv"
+transport_final_parse_file = "../parsed/final_deft_parse_transport.csv"
+sentiment_final_parse_file = "../parsed/final_deft_parse_sentiment.csv"
+transport_nb_classifier_file = "../classifiers/deft_nb_classifier_transport.pickle"
+sentiment_nb_classifier_file = "../classifiers/deft_nb_classifier_sentiment.pickle"
 
 parser = Parser()
 
-# parser.generate_wordset(data_filename=dataset_file,
-#                         word_filename=wordset_file)
-#
-# parser.generate_charset(data_filename=dataset_file,
-#                         char_filename=charset_file)
-#
-# parser.generate_first_parse(data_filename=dataset_file,
-#                             stopwords_filename=stopwords_file,
-#                             sentiments_filename=sentiments_file,
-#                             parsed_filename=parsed_file,
-#                             alphabet_filename=alphabet_file)
-#
-# parser.first_dictionary(parsed_filename=parsed_file,
-#                         dict_filename=dictionary_file)
-#
-# parser.second_parse(parse_filename=parsed_file,
-#                     final_parse_filename=final_parse_file,
-#                     updated_dict_filename=updated_dictionary_file)
+parser.generate_wordset(data_filename=dataset_file,
+                        word_filename=wordset_file)
 
-parser.generate_featuresrets(parsed_filename=final_parse_file,
-                             dict_filename=updated_dictionary_file,
-                             featuresets_filename=featuresets_file,
-                             sentiments_filename=sentiments_file)
+parser.generate_charset(data_filename=dataset_file,
+                        char_filename=charset_file)
 
-parser.closefiles()
+parser.generate_first_parse(data_filename=dataset_file,
+                            stopwords_filename=stopwords_file,
+                            parsed_filename=transport_parse_file,
+                            alphabet_filename=alphabet_file,
+                            is_transport=True)
+
+parser.generate_first_parse(data_filename=dataset_file,
+                            stopwords_filename=stopwords_file,
+                            parsed_filename=sentiment_parse_file,
+                            alphabet_filename=alphabet_file,
+                            is_transport=False)
+
+parser.generate_dictionary(parsed_filename=transport_parse_file,
+                           dict_filename=transport_dictionary_file,
+                           is_transport=True)
+
+parser.generate_dictionary(parsed_filename=sentiment_parse_file,
+                           dict_filename=sentiment_dictionary_file,
+                           is_transport=False)
+
+parser.generate_second_parse(parse_filename=transport_parse_file,
+                             final_parse_filename=transport_final_parse_file,
+                             updated_dict_filename=transport_updated_dictionary_file,
+                             is_transport=True)
+
+parser.generate_second_parse(parse_filename=sentiment_parse_file,
+                             final_parse_filename=sentiment_final_parse_file,
+                             updated_dict_filename=sentiment_updated_dictionary_file,
+                             is_transport=False)
+#
+# parser.learn_naive_bayes(parsed_filename=transport_final_parse_file,
+#                          dict_filename=transport_updated_dictionary_file,
+#                          nb_classifier_filename=transport_nb_classifier_file)
+#
+# parser.learn_naive_bayes(parsed_filename=sentiment_final_parse_file,
+#                          dict_filename=sentiment_updated_dictionary_file,
+#                          nb_classifier_filename=sentiment_nb_classifier_file)
